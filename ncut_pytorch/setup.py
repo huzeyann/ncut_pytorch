47f1617ccdb46c54f93c093b5caf8f35d1be8509
huze <huze.yann@gmail.com>
2024-11-07 22:40:04 +0000

    add lab color space

--- ncut_pytorch/setup.py
+++ ncut_pytorch/setup.py
@@ -14,7 +14,7 @@ with open(requirement_file) as f:
     
 setup(
     name="ncut_pytorch",
-    version="1.7.8",
+    version="1.7.9",
     packages=['ncut_pytorch'],
     long_description=long_description,
     long_description_content_type='text/markdown',
