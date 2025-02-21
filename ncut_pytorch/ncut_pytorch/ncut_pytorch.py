a0f6de57b512e8d4db17015e940f81e0a459a6c2
huze <huze.yann@gmail.com>
2025-02-21 16:40:59 +0000

    add find gamma

--- ncut_pytorch/ncut_pytorch/ncut_pytorch.py
+++ ncut_pytorch/ncut_pytorch/ncut_pytorch.py
@@ -502,6 +502,36 @@ def gram_schmidt(matrix):
     return orthogonal_matrix
 
 
+def find_gamma_by_degree(features, target_degree, feature_B=None, 
+                    distance='cosine', init_gamma=0.5, r_tol=1e-2, max_iter=100):
+    # features: (n_samples, n_features)
+    # binary search for optimal gamma, such that the mean edge weight is close to target_d
+    # TODO: change default distance to 'rbf' for both this and ncut_fn
+    gamma = init_gamma
+    mean_degree = affinity_from_features(features, features_B=feature_B, distance=distance, affinity_focal_gamma=gamma).mean()
+    i_iter = 0
+    low, high = 0, float('inf')
+    tol = r_tol * target_degree
+    while abs(mean_degree - target_degree) > tol and i_iter < max_iter:
+        if mean_degree > target_degree:
+            high = gamma
+            gamma = (low + gamma) / 2
+        else:
+            low = gamma
+            gamma = gamma * 2 if high == float('inf') else (gamma + high) / 2
+        mean_degree = affinity_from_features(features, features_B=feature_B, distance=distance, affinity_focal_gamma=gamma).mean()
+        i_iter += 1
+    return gamma
+
+
+def find_gamma_by_degree_after_fps(features, target_degree, distance='cosine', init_gamma=0.5, r_tol=1e-2, max_iter=100, num_sample=1000):
+    # features: (n_samples, n_features)
+    # binary search for optimal gamma, such that the mean edge weight is close to target_d
+    sample_indices = farthest_point_sampling(features, num_sample)
+    sampled_features = features[sample_indices]
+    return find_gamma_by_degree(sampled_features, target_degree, distance=distance, init_gamma=init_gamma, r_tol=r_tol, max_iter=max_iter)
+
+
 def correct_rotation(eigen_vector):
     # correct the random rotation (flipping sign) of eigenvectors
     rand_w = torch.ones(
